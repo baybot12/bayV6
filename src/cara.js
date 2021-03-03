1612a2024541bfe103f475656f1e0268c04075d7
baybot12 <78244605+baybot12@users.noreply.github.com>
2021-03-03 22:59:26 +0700

    Update cara.js

--- src/cara.js
+++ src/cara.js
@@ -8,19 +8,19 @@ Kalo gak paham tuh baca dulu -_-
 ➸ *${prefix}sticker*
 Kirim gambar/video dengan caption sticker
 ➸ *${prefix}ttp*
-Text to sticker contoh : ${prefix}ttp Lann ID
+Text to sticker contoh : ${prefix}ttp abay ID
 ➸ *${prefix}tts*
-Suara google contoh : ${prefix}tts id Hallo Ramlan
+Suara google contoh : ${prefix}tts id Hallo abay
 ➸ *${prefix}toimg*
 Reply sticker yang mau dijadiin image
 ➸ *${prefix}nulis*
 Untuk menulis dibuku
 ➸ *${prefix}stalkig*
-Stalking instagram contoh : ${prefix}stalkig iamramlan_
+Stalking instagram contoh : ${prefix}stalkig quotes.abay05
 ➸ *${prefix}quotes*
 Random quotes
 ➸ *${prefix}bikinquote*
-Membuat quotes contoh : ${prefix}bikinquote Kamu gay & Ramlan
+Membuat quotes contoh : ${prefix}bikinquote Kamu gay & abay
 ➸ *${prefix}play*
 Mendownload lagu dari youtube bisa menggunakan text atau link
 ➸ *${prefix}yutubdl*
@@ -30,7 +30,7 @@ Mendownload video tiktok, gunakan link kak
 ➸ *${prefix}hartatahta*
 Membuat lomgo, contoh : ${prefix}hartatahta Nadia
 ➸ *${prefix}pornhub*
-Membuat logo, contoh : ${prefix}pornhub Ramlan & Hub
+Membuat logo, contoh : ${prefix}pornhub orng & Hub
 Note : untuk logo maker / image maker jika undefined berarti harus memakai &
 ➸ *${prefix}fitnah*
 Only grup untuk fitnah orang, contoh : ${prefix}fitnah @tagtarget & Hai & Hai juga
@@ -39,7 +39,7 @@ Gacha nomor yang ada di database / yang menggunakan bot
          ────────────────
 Note : Sisanya gunain otak kalian aja cape w jelasin :)
          ────────────────
-🔰 -----[ *TUMTOR BY RAMLAN ID* ]----- 🔰
+🔰 -----[ *TUMTOR BY  ID* ]----- 🔰
 `
 }
 
